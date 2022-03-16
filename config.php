8fb2e23299d337c3ac5b6016ca6c6a93971fdcc5
EvgeniyZav <zavevgeniy@yandex.ru>
2022-03-16 13:50:26 +0500

    Version 1.0.0

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 const DB_HOSTNAME = '89.108.83.110';
-const DB_USERNAME = 'is342_eremin';
-const DB_PASSWORD = 'testingmysql';
-const DB_DATABASE = 'is342_eremin';
+const DB_USERNAME = 'student_Evgeniy';
+const DB_PASSWORD = '1337115';
+const DB_DATABASE = 'student_7';
 const DB_PORT = '3306'; # Default 3306
